7d735dcbc2bc191f6a8caa4929ed14db51918997
Sabelli <sabellinho@gmail.com>
2026-09-13 15:54:03 -0300

    chore: remove arquivos base e organiza diretórios em clean arch

---
+++ Jogos.API/Infrastructure/Data/ApplicationContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Jogos.API.Infrastructure.Data
+{
+    public class ApplicationContext:DbContext
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options)
+        {
+
+        }
+    }
+}
