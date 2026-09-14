3138c540d8887a3b09c3f0390cca66bd844b3a64
Sabelli <sabellinho@gmail.com>
2026-09-13 21:02:22 -0300

    fix: diminui mínimo c_nome Plataforma

--- Jogos.API/Application/Dtos/PlataformaRequestDto.cs
+++ Jogos.API/Application/Dtos/PlataformaRequestDto.cs
@@ -5,7 +5,7 @@ namespace Jogos.API.Application.Dtos
     public class PlataformaRequestDto
     {
         [Required(ErrorMessage = "O nome da plataforma é obrigatório")]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome da plataforma deve ter entre 3 e 100 caracteres")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "O nome da plataforma deve ter entre 2 e 100 caracteres")]
         public string Nome { get; set; }
     }
 }
--- Jogos.API/Domain/Entities/PlataformaEntity.cs
+++ Jogos.API/Domain/Entities/PlataformaEntity.cs
@@ -13,7 +13,7 @@ namespace Jogos.API.Domain.Entities
         public int Id { get; set; }
 
         [Required(ErrorMessage = "O nome da plataforma é obrigatório")]
-        [StringLength(100, MinimumLength = 3, ErrorMessage = "O nome da plataforma deve ter entre 3 e 100 caracteres")]
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "O nome da plataforma deve ter entre 2 e 100 caracteres")]
         [Column("c_nome")]
         public string Nome { get; set; }
 
